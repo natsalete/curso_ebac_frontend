7fa93a724be14b6a2152a6b7592e17018155c53a
natalete <natsalete14@gmail.com>
2024-04-19 16:22:52 -0300

    feat: adição da atividade proposta

---
+++ Atv.js
@@ -0,0 +1,16 @@
+
+/*Neste exercício você vai praticar a construção de funções com o JavaScript.
+
+Você deve criar uma função chamada "calculaArea".
+Ela deverá receber dois parâmetros:
+1) base
+2) altura
+Deverá retornar a multiplicação destes fatores.
+
+Copie o código da função que você escreveu na Plataforma da EBAC que nossos tutores vão avaliar e dar o feedback.*/
+
+function calculaArea(base, altura){
+    return base * altura
+}
+
+calculaArea(10,2)
